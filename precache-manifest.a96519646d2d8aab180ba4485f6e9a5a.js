6327087a6c639b9f01d63e2c8f6a2512a44b983d
Matt Stedman <67828580+Matt-Stedman@users.noreply.github.com>
2023-01-22 10:47:38 +0000

    Updates

---
+++ precache-manifest.a96519646d2d8aab180ba4485f6e9a5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6451ff699583a3cb3cf5",
+    "url": "/sensai-planner/static/css/main.152edeb7.chunk.css"
+  },
+  {
+    "revision": "6451ff699583a3cb3cf5",
+    "url": "/sensai-planner/static/js/main.6451ff69.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/sensai-planner/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "654739b91834312ef4ee",
+    "url": "/sensai-planner/static/js/2.654739b9.chunk.js"
+  },
+  {
+    "revision": "28997f382e08a1fe78be",
+    "url": "/sensai-planner/static/js/runtime~main.28997f38.js"
+  },
+  {
+    "revision": "f952e060a3f173bc8f5938adbf98955b",
+    "url": "/sensai-planner/index.html"
+  }
+];
